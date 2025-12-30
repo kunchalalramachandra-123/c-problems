9916f91368b7fe93bd2cdaa07f36c9d40cdbf829
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2025-12-30 22:50:12 +0530

    Add files via upload

---
+++ 30-12-25 10..c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int m;
+    scanf("%d",&m);
+    if (m >= 75)
+    printf("G A");
+    else if (m >= 50)
+    printf("G B");
+    else
+    printf("G C");
+    return 0;
+}
