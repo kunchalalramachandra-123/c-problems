0f8f18f8753c7fe9c5edc3d1f8c6a6b12670a5ab
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2025-12-30 22:48:30 +0530

    Add files via upload

---
+++ 30-12-25 6..c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    char ch;
+    scanf("%c",&ch);
+    if (ch=='a'||ch=='e'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='o'||ch=='U')
+    printf("Vowel");
+    else
+    printf("Not Vowel");
+
+    
+    return 0;
+}
