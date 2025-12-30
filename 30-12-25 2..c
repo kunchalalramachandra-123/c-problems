c37bf26658bbd05c17c015a1196c06effb59f840
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2025-12-30 22:47:03 +0530

    Add files via upload

---
+++ 30-12-25 2..c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    if (n>0)
+    printf("p");
+    else if(n<0)
+    printf("n");
+    else
+    printf("z");
+    
+    return 0;
+}
