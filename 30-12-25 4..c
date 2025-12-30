22460f7b3fab2b51370aec9890c32705ac34167f
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2025-12-30 22:47:51 +0530

    Add files via upload

---
+++ 30-12-25 4..c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int a,b,c;
+    scanf("%a %b %c",&a,&b,&c);
+    if(a>=b && b>=c)
+  printf("%d",a);
+  else if (b>=a &&b>=c)
+  printf("%d",b);
+  else
+  printf("%d",c);
+    return 0;
+}
