96db34f7ba2166f9f346a8ab4d5a09555ab75c69
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2026-01-03 23:26:54 +0530

    Add files via upload

---
+++ 3-1-26 2,.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+    int n,temp, rev =0;
+    scanf("%d",&n);
+    temp =n;
+    while(n =10){
+        rev = rev * 10 + n % 10;
+        n /= 10;
+    }
+    if  (temp == rev)
+    printf("P");
+    else
+    printf("N P");
+
+    return 0;
+}
