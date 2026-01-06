ecea04d1fbcb1609bbeec556503dd3e2af411fe4
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2026-01-06 23:23:02 +0530

    Add files via upload

---
+++ 6-1-26 2..c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+     int n,a[100],sum = 0;
+     scanf("%d",&n);
+     for(int i=0; i < n; i++ ){
+        scanf("%d",&a[i]);
+        sum += a[i];
+     }
+    printf("%d",sum);
+    return 0;
+}
