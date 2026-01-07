de9feaeb663ed72159eb63b0dea3f5619c258888
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2026-01-07 23:36:39 +0530

    Add files via upload

---
+++ 7-1-26 1..c
@@ -0,0 +1,15 @@
+#include <stdio.h>    
+int main() {
+     int n,a[100],min;
+     scanf("%d",&n);
+     for(int i=0;i < n; i++){
+        scanf("%d",&a[i]);
+     }
+     min - a[0];
+     for(int i=1; i < n;i++){
+     if(a[i] < min)
+     min = a[i];
+     }
+     printf("%d",min);
+    return 0;
+}
