03d465074817ff745ea15628850cc43c01d8e144
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2026-01-14 22:10:45 +0530

    Add files via upload

---
+++ 14-1-26 1,.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+        char s[50];
+        int i = 0;
+        scanf("%s",s);
+        while(s[i] != '\0')
+        i++;
+        printf("%d",i);
+    
+    return 0;
+}
