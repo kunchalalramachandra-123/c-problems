ab8587246696c6c51e986167d5649724f2b859fe
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2026-01-14 22:11:11 +0530

    Add files via upload

---
+++ 14-1-26 2..c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    char s1[50],s2[50];
+    int i = 0;
+    scanf("%s",s1);
+    while(s1[i] != '\0'){
+        s2[i] = s1[i];
+        i++;
+    }
+    s2[i] = '\0';
+    printf("%s",s2);
+    return 0;
+}
