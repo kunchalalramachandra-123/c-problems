8cbb71d6c53cc3d1599592ed2b679cad524acea9
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2026-01-15 22:37:12 +0530

    Add files via upload

---
+++ 15-1-26.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdio.h>
+
+int main() {
+    char s1[100],s2[100];
+    scanf("%s %s",s1,s2);
+    if (s1, s2)  
+    printf("Equal");
+    else
+    printf("Not Equal");
+    return 0;
+}
