065db7d27003f2dafdad65a72ddbc98766fa4838
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2026-01-29 22:08:06 +0530

    Add files via upload

---
+++ 29-1-26.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, fact = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    while (n > 0) {
+        fact *= n;
+        n--;
+    }
+
+    printf("Factorial = %d", fact);
+    return 0;
+}
