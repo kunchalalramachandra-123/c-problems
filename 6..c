812a9e7aa9361b7e3a69c42d94a8cddc3447b0ab
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2026-02-04 22:33:26 +0530

    Add files via upload

---
+++ 6..c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() 
+   { double temp;
+    scanf("%lf",&temp);
+    
+    if  (temp>=30 ){
+        printf("hot");
+    }
+        else 
+            printf("normal");
+            
+    
+    return 0;
+}
---
+++ 7..c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int N;
+    scanf("%d",&N);
+    if (N<=12)
+    {
+    printf("not a single digit");
+    }    
+    else
+    printf("N");
+    return
