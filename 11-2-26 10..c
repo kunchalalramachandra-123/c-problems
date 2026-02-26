a58896b0dcb39ed2389e4c117c4c2934f8d27f20
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2026-02-26 20:42:12 +0530

    Add files via upload

---
+++ 11-2-26 10..c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+ int A, B, C;
+scanf("%d %d %d", &A, &B, &C);
+ if (A >= B && A >= C)
+printf("%d", A);
+else if (B >= A && B >= C) 
+printf("%d", B);
+else 
+printf("%d", C);
+ return 0;
+}
