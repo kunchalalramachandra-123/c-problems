583e929e83d4edb37ec62abe25c4fc7afc3f27be
kunchalalramachandra-123 <kunchalalramachandra@gmail.com>
2026-04-04 20:14:29 +0530

    Add files via upload

---
+++ 4 1C..c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main() {
+   int n,sum=0;
+   scanf("%d",&n);
+   for(int i=1;i<n;i++){
+ if(n%i==0){
+    sum+=i;
+ } 
+   } 
+   if(sum==n){
+    printf("%d is the perfect number",n);
+   }
+    return 0;
+}
